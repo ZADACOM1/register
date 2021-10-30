fc015c04bade44d874fc8c3ee749216f815deb4f
ZadaCom1 <zadagamerytcnl@gmail.com>
2021-10-30 09:10:19 +0700

    register

---
+++ coba2-laravel/resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('My Tutor Blog - Dashboard') }}</div>
+  
+                <div class="card-body">
+                    
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('success') }}
+                        </div>
+                    
+  
+                    <h3>Welcome</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
